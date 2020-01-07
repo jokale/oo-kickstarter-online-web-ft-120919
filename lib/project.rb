aac06f1ae0fa6be804390cbf52a7be7c837d9209
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 19:44:09 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 class Project
-  attr_reader :name, :title 
+  attr_reader  :title 
   
-  def initialize(name)
-    @name= name
+  def initialize(title)
+    @title = title
     @backer = []
   end 
 end 
